13cdae822cc2a66eacde9caeda6960917ef271cc
emilych7 <emilyhernandezBVT2023@gmail.com>
2023-10-07 14:52:05 -0700

    more globePoints

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -35,3 +35,7 @@ wwd.addLayer(globePoint(24.7143, 58.7374, "Gulf of Oman"));
 wwd.addLayer(globePoint(37.6640, 127.9785, "Korean Peninsula"));
 wwd.addLayer(globePoint(64.9631, -19.0208, "Iceland"));
 wwd.addLayer(globePoint(26.7313, -110.7122, "Gulf of California"));
+wwd.addLayer(globePoint(58.4880, 19.8633, "Baltic Sea"));
+wwd.addLayer(globePoint(-39.3724, 177.3016, "Hawke Bay, New Zealand"));
+wwd.addLayer(globePoint(-35.1945, -56.7412, "Rio de La Plata, Uruguay"));
+wwd.addLayer(globePoint(-12, 13, "Benegula Current, South Africa"));
