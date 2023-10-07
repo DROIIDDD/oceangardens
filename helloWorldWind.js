258a9abd48f025053bffe04b9f000a05991ff327
emilych7 <emilyhernandezBVT2023@gmail.com>
2023-10-07 14:36:36 -0700

    adding several globePoints

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -8,19 +8,19 @@ function globePoint(latitude, longitude, city) {
     
     placemarkAttributes.labelAttributes.color = WorldWind.Color.WHITE;
     placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-                WorldWind.OFFSET_FRACTION, 0.5,
-                WorldWind.OFFSET_FRACTION, 1.0);
-    
-                placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
-    
-                var position = new WorldWind.Position(latitude, longitude, 100.0);
-                var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+    WorldWind.OFFSET_FRACTION, 0.5,
+    WorldWind.OFFSET_FRACTION, 1.0);
 
-                placemark.label = city + "\n";
-            placemark.alwaysOnTop = true;          
-    
-            placemarkLayer.addRenderable(placemark);
-            return placemarkLayer;
+    placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
+
+    var position = new WorldWind.Position(latitude, longitude, 100.0);
+    var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+
+    placemark.label = city + "\n";
+    placemark.alwaysOnTop = true;          
+
+    placemarkLayer.addRenderable(placemark);
+    return placemarkLayer;
 }
 
 var wwd = new WorldWind.WorldWindow("canvasOne");
@@ -29,4 +29,9 @@ wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
-wwd.addLayer(globePoint(35.6762, 139.6503, "Tokyo")); // Tokyo     
+wwd.addLayer(globePoint(35.6762, 139.6503, "Tokyo"));
+wwd.addLayer(globePoint(42.0669, -81.3399, "Lake Eerie"));
+wwd.addLayer(globePoint(24.7143, 58.7374, "Gulf of Oman"));
+wwd.addLayer(globePoint(37.6640, 127.9785, "Korean Peninsula"));
+wwd.addLayer(globePoint(64.9631, -19.0208, "Iceland"));
+wwd.addLayer(globePoint(26.7313, -110.7122, "Gulf of California"));
